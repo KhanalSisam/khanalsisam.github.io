e6ca4ddb029318055396570454e1d7dd215d8ea6
KhanalSisam <54133474+KhanalSisam@users.noreply.github.com>
2021-11-24 19:28:35 +0545

    added "." to at the front of paths in addAll

--- Paw/sw.js
+++ Paw/sw.js
@@ -4,17 +4,17 @@ self.addEventListener("install", (event) => {
         caches.open("v1").then(cache => {
             console.log("cache opened")
             return cache.addAll([
-                "/pages/home/home.js",
-                "/pages/home/home.css",
-                "/pages/home/home.html",
-                "/pages/text/text.css",
-                "/pages/text/text.html",
-                "/pages/text/text.js",
-                "/pages/text/check.js",
-                "/text.json",
-                "/indexedDB.js",
-                "/assets/pawprint192x192.svg",
-                "/assets/pawprint512x512.svg",
+                "./pages/home/home.js",
+                "./pages/home/home.css",
+                "./pages/home/home.html",
+                "./pages/text/text.css",
+                "./pages/text/text.html",
+                "./pages/text/text.js",
+                "./pages/text/check.js",
+                "./text.json",
+                "./indexedDB.js",
+                "./assets/pawprint192x192.svg",
+                "./assets/pawprint512x512.svg",
             ])
         })
     )
